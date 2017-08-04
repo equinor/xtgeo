e0309f3272cc80fd34a11e5d16ddc5ed3bb93c74
Jan C. Rivenaes <JRIV@statoil.com>
2017-08-04 09:18:01 +0200

    Removed _version.py from git

--- xtgeo/_version.py
+++
@@ -1,6 +0,0 @@
-
-# This file is originally generated from Git information by running:
-# 'setup.py version'. Distribution tarballs contain a pre-generated
-# copy of this file.
-__version__ = '0.9.0'
-_xtgeo_build = '0.9.0'
