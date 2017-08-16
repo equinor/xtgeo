a02ff66da0dae2c07d1c403371dddfa6ad940806
Jan C. Rivenæs <jriv@statoil.com>
2017-08-16 10:10:19 +0200

    Another attempt for Agg backend determination on ERT, adding another env variable...

--- xtgeo/__init__.py
+++ xtgeo/__init__.py
@@ -3,10 +3,16 @@ import os
 
 # to avoid problems in batch runs when no DISPLAY is set:
 import matplotlib as mplib
-display = os.environ.get('DISPLAY','')
-host = os.environ.get('HOSTNAME','')
-print("="*79)
-if display == '' or 'grid' in host or 'lgc' in host:
+display = os.environ.get('DISPLAY', '')
+host1 = os.environ.get('HOSTNAME', '')
+host2 = os.environ.get('HOST', '')
+host = host1 + host2
+
+print('')
+print('=' * 79)
+if display == '' or 'grid' in host1 or 'lgc' in host1 or\
+   'LSB_JOBID' in os.environ:
+
     print('XTGeo info: No display found or ERT server. Using non-interactive '
           'Agg backend for matplotlib')
     mplib.use('Agg')
@@ -14,7 +20,7 @@ else:
     print('XTGeo info: Matplotlib Agg backend is NOT active, '
           'allow interactive plots')
 
-print("="*79)
+print('=' * 79)
 
 from .surface import regular_surface
 from .grid3d import grid
