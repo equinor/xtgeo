94e2eff1782792e4d68cd6c0c00bdc7a01b6c817
Jan C. Rivenaes <JRIV@statoil.com>
2018-03-04 21:01:30 +0100

    Improvings tests for roxapi in test_surface

--- tests/test_surface/test_regular_surface_roxapi.py
+++ tests/test_surface/test_regular_surface_roxapi.py
@@ -1,16 +1,20 @@
 import os
-import os.path
-import sys
 
-from xtgeo.surface import RegularSurface
+import xtgeo
 from xtgeo.common import XTGeoDialog
 import test_common.test_xtg as tsetup
+
+try:
+    import roxar
+    roxver = roxar.__version__
+except ImportError:
+    pass
 
 xtg = XTGeoDialog()
 logger = xtg.basiclogger(__name__)
 
 if not xtg.testsetup():
-    sys.exit(-9)
+    raise SystemExit
 
 td = xtg.tmpdir
 testpath = xtg.testpath
@@ -18,21 +22,52 @@ testpath = xtg.testpath
 # =============================================================================
 # Do tests
 # =============================================================================
+proj = {}
+proj['1.1'] = '../xtgeo-testdata-statoil/data/rmsprojects/reek.rms10.1.1'
 
 
 @tsetup.skipunlessroxar
 def test_getsurface():
     """Get a surface from a RMS project."""
 
-    logger.info('Simple case...')
+    if not os.path.isdir(proj[roxver]):
+        raise RuntimeError('RMS test project is missing for roxar version {}'
+                           .format(roxver))
 
-    project = "/private/jriv/tmp/fossekall/fossekall.rms10.1.1"
+    logger.info('Simple case, reading a surface from a Horizons category')
 
-    x = RegularSurface()
-    x.from_roxar(project, name='TopIle', category="DepthSurface")
+    x = xtgeo.surface.RegularSurface()
+    x.from_roxar(proj[roxver], 'TopUpperReek', 'DS_extracted')
 
-    x.to_file("TMP/topile.gri")
+    x.to_file(os.path.join(td, 'topupperreek_from_rms.gri'))
 
-    tsetup.assert_equal(x.ncol, 273, "NCOL of top Ile from RMS")
+    tsetup.assert_equal(x.ncol, 99, 'NCOL of from RMS')
 
-    tsetup.assert_almostequal(x.values.mean(), 2771.82236, 0.001)
+    tsetup.assert_almostequal(x.values.mean(), 1696.255599, 0.001)
+
+
+@tsetup.skipunlessroxar
+def test_getsurface_from_zones():
+    """Get a surface from a RMS project, from the zones container."""
+
+    if not os.path.isdir(proj[roxver]):
+        raise RuntimeError('RMS test project is missing for roxar version {}'
+                           .format(roxver))
+
+    logger.info('Simple case, reading a surface from a Horizons category')
+
+    # direct initiate an instance from Roxar import
+    x = xtgeo.surface_from_roxar(proj[roxver], 'UpperReek', 'IS_calculated',
+                                 stype='zones')
+
+    x.to_file(os.path.join(td, 'upperreek_from_rms.gri'))
+
+    tsetup.assert_equal(x.ncol, 99, 'NCOL of from RMS')
+
+    # values from mean and stddev are read from statistics in RMS
+    tsetup.assert_almostequal(x.values.mean(), 20.8205, 0.001)
+    tsetup.assert_almostequal(x.values.std(), 1.7867, 0.001)
+    print(x.values.std())
+
+    # write to folder
+    x.to_roxar(proj[roxver], 'UpperReek', 'IS_jriv', stype='zones')
