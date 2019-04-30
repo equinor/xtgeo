5b45ffcfe1b396a1b91a8448920aad59017175d2
Jan C. Rivenaes <JRIV@statoil.com>
2019-04-30 13:07:49 +0200

    WIP: Travis setup

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -9,11 +9,12 @@ tar xzvf swig-3.0.12.tar.gz
 cd swig-3.0.12
 sh ./configure; make; make install
 
-# # Compile wheels
-# for PYBIN in /opt/python/*/bin; do
-#     "${PYBIN}/pip" install -r /io/dev-requirements.txt
-#     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
-# done
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" install -r /io/requirements.txt
+    "${PYBIN}/pip" install -r /io/requirements_dev.txt
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
 
 # # Bundle external shared libraries into the wheels
 # for whl in wheelhouse/*.whl; do
