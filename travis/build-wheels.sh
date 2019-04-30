d45b1ea1f157c05bf48f5fe115b537f937004eb2
Jan C. Rivenaes <JRIV@statoil.com>
2019-04-30 14:35:47 +0200

    WIP: Travis setup......

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -11,14 +11,21 @@ sh ./configure > /dev/null
 make > /dev/null
 make install > /dev/null
 
+export PYHOME=/home
+cd ${PYHOME}
+
+/opt/python/cp36-cp36m/bin/pip install twine cmake
+ln -s /opt/python/cp36-cp36m/bin/cmake /usr/bin/cmake
+
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
     echo $PYBIN
-    if [[ $PYBIN == *"36"* ]]; then
+    if [[ $PYBIN == *"cp3"* ]]; then
         echo "Install for $PYBIN"
-        "${PYBIN}/pip" install -r /io/requirements.txt
-        "${PYBIN}/pip" install -r /io/requirements_dev.txt
+        "${PYBIN}/pip" install -r numpy
+        # "${PYBIN}/pip" install -r /io/requirements_dev.txt
         "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+        "${PYBIN}/python" /io/setup.py sdist -d /io/wheelhouse/
     fi
 done
 
@@ -31,4 +38,20 @@ done
 # for PYBIN in /opt/python/*/bin/; do
 #     "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
 #     (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
+# done
+
+
+# Test
+# for PYBIN in /opt/python/cp3*/bin/; do
+#     "${PYBIN}/pip" install -r /io/requirements-test.txt
+#     "${PYBIN}/pip" install --no-index -f /io/wheelhouse nb-cpp
+#     (cd "$PYHOME"; "${PYBIN}/pytest" /io/test/)
+# done
+
+# #  Upload
+# for WHEEL in /io/wheelhouse/nb_cpp*; do
+#     /opt/python/cp37-cp37m/bin/twine upload \
+#         --skip-existing \
+#         -u "${TWINE_USERNAME}" -p "${TWINE_PASSWORD}" \
+#         "${WHEEL}"
 # done
