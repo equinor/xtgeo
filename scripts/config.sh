df183bae502ed0693e5eae73f5a7aa6cea26adf0
Jan C. Rivenaes <jan@rivenaes.net>
2019-05-06 19:46:29 +0200

    Trying multibuild

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,23 +1,14 @@
+SWIGURL="https://ftp.osuosl.org/pub/blfs/conglomeration/swig"
+SWIG="swig-3.0.12"
 
 function pre_build {
-    CPWD=$PWD
-    ROOT="/"
-    TMP="/tmp"
-    IO="/io"
-
-    SWIGURL="https://ftp.osuosl.org/pub/blfs/conglomeration/swig"
-    SWIG="swig-3.0.12"
-
-    # Install the SWIG package required by our library for install
-    cd $TMP
 
     yum install -y pcre-devel
     curl -O ${SWIGURL}/${SWIG}.tar.gz
     tar xzf ${SWIG}.tar.gz
-    cd $SWIG
+    pushd $SWIG
     sh ./configure > /dev/null
     make > /dev/null
     make install > /dev/null
-    cd $CPDW
-    echo "PWD is $PWD"
+    popd
 }
