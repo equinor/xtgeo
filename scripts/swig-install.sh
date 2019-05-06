2354b52076d46310f0fce821391a1cedcb254f37
Jan C. Rivenaes <jan@rivenaes.net>
2019-05-06 18:53:00 +0200

    Trying multibuild

---
+++ scripts/swig-install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e -x
+
+ROOT="/"
+TMP="/tmp"
+IO="/io"
+
+SWIGURL="https://ftp.osuosl.org/pub/blfs/conglomeration/swig"
+SWIG="swig-3.0.12"
+
+# Install the SWIG package required by our library for install
+cd $TMP
+
+yum install -y pcre-devel
+curl -O ${SWIGURL}/${SWIG}.tar.gz
+tar xzf ${SWIG}.tar.gz
+cd $SWIG
+sh ./configure > /dev/null
+make > /dev/null
+make install > /dev/null
