a923ae588114bc0f6e7e3823e1af887de7eebd4b
Jan C. Rivenaes <JRIV@statoil.com>
2019-05-13 11:29:27 +0200

    Set up travis with multibuild approach

--- scripts/config.sh
+++ scripts/config.sh
@@ -3,22 +3,19 @@ SWIG="swig-3.0.12"
 
 function pre_build {
 
-    yum install -y pcre-devel
-    curl -O ${SWIGURL}/${SWIG}.tar.gz
-    tar xzf ${SWIG}.tar.gz
-    pushd $SWIG
-    sh ./configure > /dev/null
-    make > /dev/null
-    make install > /dev/null
-    popd
+    echo "Prebuild stage, install swig..."
+    build_swig
 }
 
 function run_tests {
     set -x
+    apt-get -y update > /dev/null
+    apt-get -y install git > /dev/null
+    git clone --depth 1 https://github.com/equinor/xtgeo-testdata ../../xtgeo-testdata
     ls -l ..
     ls -l ../..
     pip install pytest
     python -c "import xtgeo; print(xtgeo.__version__)"
-    ln -s ../tests tests
-    pytest tests/test_simple
+    pushd ..
+    pytest tests
 }
