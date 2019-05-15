74095ee1707079cdbf0b3e11329cc1d69ad78669
Jan C. Rivenaes <jan@rivenaes.net>
2019-05-15 18:25:12 +0200

    Added all tests in setup, and badges in README

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,21 +1,18 @@
-SWIGURL="https://ftp.osuosl.org/pub/blfs/conglomeration/swig"
-SWIG="swig-3.0.12"
 
 function pre_build {
-
     echo "Prebuild stage, install swig..."
     build_swig
 }
 
 function run_tests {
     set -x
+    # need to install git on the test docker image to get the test data
     apt-get -y update > /dev/null
     apt-get -y install git > /dev/null
     git clone --depth 1 https://github.com/equinor/xtgeo-testdata ../../xtgeo-testdata
-    ls -l ..
-    ls -l ../..
     pip install pytest
+
     python -c "import xtgeo; print(xtgeo.__version__)"
     pushd ..
-    pytest tests/test_well
+    pytest tests
 }
