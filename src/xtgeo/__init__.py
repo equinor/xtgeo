330bf9d1f43bb93cd2233e47dc0777eb53ab955b
Jan C. Rivenaes <JRIV@statoil.com>
2019-05-23 08:10:14 +0200

    Added more imports in __init__

--- src/xtgeo/__init__.py
+++ src/xtgeo/__init__.py
@@ -10,9 +10,11 @@ import timeit
 
 try:
     from ._theversion import version
+
     __version__ = version
 except ImportError:
-    __version__ = '0.0.0'
+    __version__ = "0.0.0"
+
 
 def _timer(*args):
     time1 = timeit.default_timer()
@@ -22,6 +24,7 @@ def _timer(*args):
 
     return time1
 
+
 TIME0 = _timer()
 
 DEBUG = 19
@@ -29,42 +32,44 @@ DEBUG = 19
 if os.environ.get("XTG_VERBOSE_LEVEL") is None:
     DEBUG = 0
 
+
 def _xprint(msg):
 
     difftime = _timer(TIME0)
 
     if DEBUG:
-        print('({0:4.3f})  {1}'.format(difftime, msg))
+        print("({0:4.3f})  {1}".format(difftime, msg))
 
-_xprint('XTGEO __init__ ...')
+
+_xprint("XTGEO __init__ ...")
 
 # to avoid problems in batch runs when no DISPLAY is set:
-_xprint('Import matplotlib etc...')
+_xprint("Import matplotlib etc...")
 import matplotlib as mplib
 
-display = os.environ.get('DISPLAY', '')
-host1 = os.environ.get('HOSTNAME', '')
-host2 = os.environ.get('HOST', '')
+display = os.environ.get("DISPLAY", "")
+host1 = os.environ.get("HOSTNAME", "")
+host2 = os.environ.get("HOST", "")
 dhost = host1 + host2 + display
 
-ertbool = 'LSB_JOBID' in os.environ
+ertbool = "LSB_JOBID" in os.environ
 
-if display == '' or 'grid' in dhost or 'lgc' in dhost or ertbool:
+if display == "" or "grid" in dhost or "lgc" in dhost or ertbool:
 
-    _xprint('')
-    _xprint('=' * 79)
+    _xprint("")
+    _xprint("=" * 79)
 
     _xprint(
-        'XTGeo info: No display found or a batch (e.g. ERT) server. '
-        'Using non-interactive Agg backend for matplotlib'
+        "XTGeo info: No display found or a batch (e.g. ERT) server. "
+        "Using non-interactive Agg backend for matplotlib"
     )
-    mplib.use('Agg')
-    _xprint('=' * 79)
+    mplib.use("Agg")
+    _xprint("=" * 79)
 
 #
 # Order matters!
 #
-_xprint('Import matplotlib etc...DONE')
+_xprint("Import matplotlib etc...DONE")
 
 from xtgeo.common.constants import UNDEF
 from xtgeo.common.constants import UNDEF_LIMIT
@@ -78,9 +83,9 @@ from xtgeo.common.exceptions import WellNotFoundError
 from xtgeo.common.exceptions import GridNotFoundError
 from xtgeo.common.exceptions import BlockedWellsNotFoundError
 
-_xprint('Import common... done')
+_xprint("Import common... done")
 
-_xprint('Import various XTGeo modules...')
+_xprint("Import various XTGeo modules...")
 
 from xtgeo.roxutils import roxutils
 from xtgeo.roxutils.roxutils import RoxUtils
@@ -89,36 +94,54 @@ from xtgeo.well import well1
 from xtgeo.well import wells
 from xtgeo.well import blocked_well
 from xtgeo.well import blocked_wells
-_xprint('Import various XTGeo modules... wells...')
+
+from xtgeo.well.well1 import Well
+from xtgeo.well.wells import Wells
+from xtgeo.well.blocked_well import BlockedWell
+from xtgeo.well.blocked_wells import BlockedWells
+
+_xprint("Import various XTGeo modules... wells...")
 
 from xtgeo.surface import regular_surface
-_xprint('Import various XTGeo modules... surface...')
+
+_xprint("Import various XTGeo modules... surface...")
 
 from xtgeo.cube import cube1
-_xprint('Import various XTGeo modules... cube...')
+
+from xtgeo.cube.cube1 import Cube
+
+_xprint("Import various XTGeo modules... cube...")
 
 from xtgeo.grid3d import grid
 from xtgeo.grid3d import grid_property
 from xtgeo.grid3d import grid_properties
-_xprint('Import various XTGeo modules... 3D grids...')
+
+from xtgeo.grid3d.grid import Grid
+from xtgeo.grid3d.grid_property import GridProperty
+from xtgeo.grid3d.grid_properties import GridProperties
+
+_xprint("Import various XTGeo modules... 3D grids...")
 
 from xtgeo.xyz import points
 from xtgeo.xyz import polygons
-_xprint('Import various XTGeo modules... xyz...')
+
+from xtgeo.xyz.points import Points
+from xtgeo.xyz.polygons import Polygons
+
+_xprint("Import various XTGeo modules... xyz...")
 
 
 from xtgeo.plot import baseplot
 from xtgeo.plot import xsection
 from xtgeo.plot import xtmap
 from xtgeo.plot import grid3d_slice
-_xprint('Import various XTGeo modules... plots...')
 
+_xprint("Import various XTGeo modules... plots...")
 
-_xprint('Import various XTGeo modules...DONE')
-
+_xprint("Import various XTGeo modules...DONE")
 
 # some function wrappers to initiate objects from imports
-_xprint('Import various XTGeo wrappers...')
+_xprint("Import various XTGeo wrappers...")
 from xtgeo.surface.regular_surface import surface_from_file
 from xtgeo.surface.regular_surface import surface_from_roxar
 from xtgeo.surface.regular_surface import surface_from_cube
@@ -147,5 +170,4 @@ from xtgeo.xyz.points import points_from_file
 from xtgeo.xyz.points import points_from_roxar
 
 
-
-_xprint('XTGEO __init__ done')
+_xprint("XTGEO __init__ done")
