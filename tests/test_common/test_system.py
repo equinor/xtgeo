95466a410ff895df0d31b0c64f4bbf41a18ac619
Jan C. Rivenaes <jan@rivenaes.net>
2019-08-28 09:37:27 +0200

    Exception in test for travis

--- tests/test_common/test_system.py
+++ tests/test_common/test_system.py
@@ -3,6 +3,7 @@ import os
 import pytest
 import xtgeo.common.xtgeo_system as xsys
 
+TRAVIS = os.environ.get("TRAVIS", False)
 
 # =============================================================================
 # Do tests of simple system functions
@@ -21,15 +22,17 @@ def test_check_folder():
     status = xsys.check_folder("src/xtgeo")
     assert status is True
 
-    folder = "TMP/nonwritable"
-    myfile = os.path.join(folder, "somefile")
-    if not os.path.exists(folder):
-        os.mkdir(folder, mode=0o440)
-    status = xsys.check_folder(myfile)
-    assert status is False
+    if not TRAVIS:
+        # skipped for travis, as travis runs with root rights
+        folder = "TMP/nonwritable"
+        myfile = os.path.join(folder, "somefile")
+        if not os.path.exists(folder):
+            os.mkdir(folder, mode=0o440)
+        status = xsys.check_folder(myfile)
+        assert status is False
 
-    status = xsys.check_folder(folder)
-    assert status is False
+        status = xsys.check_folder(folder)
+        assert status is False
 
-    with pytest.raises(ValueError):
-        xsys.check_folder(folder, raiseerror=ValueError)
+        with pytest.raises(ValueError):
+            xsys.check_folder(folder, raiseerror=ValueError)
