3af397ff30e8d963e6fc66aaa0d4c6dd908037ba
Jan C. Rivenaes <jan@rivenaes.net>
2019-10-25 15:54:52 +0200

    wip: testing on forks

--- tests/test_surface/test_forks.py
+++ tests/test_surface/test_forks.py
@@ -2,14 +2,10 @@
 from __future__ import division, absolute_import
 from __future__ import print_function
 
-import math
-from os.path import join
-
+import os
 import concurrent.futures
-import io
 
 import xtgeo
-import test_common.test_xtg as tsetup
 
 xtg = xtgeo.common.XTGeoDialog()
 logger = xtg.basiclogger(__name__)
@@ -18,9 +14,7 @@ if not xtg.testsetup():
     raise SystemExit
 
 TMPD = xtg.tmpdir
-TESTPATH = xtg.testpath
 
-TESTSET1A = "../xtgeo-testdata/surfaces/reek/1/topreek_rota.gri"
 
 def test_surface_forks():
     """Testing when surfaces are read by multiple forks"""
@@ -28,31 +22,40 @@ def test_surface_forks():
     def _get_files_as_regularsurfaces_thread(nthread):
         surfs = []
 
+        logger.info("Number of threads are %s", nthread)
         with concurrent.futures.ThreadPoolExecutor(max_workers=nthread) as executor:
 
-            futures = {executor.submit(_get_regsurf, i): i for i in range(0, nthread)}
+            futures = {executor.submit(_get_regsurf, i): i for i in range(nthread)}
 
-            for future in concurrent.futures.as_completed(futures):
-                try:
-                    surf = future.result()
-                except Exception as exc:
-                    logger.error(f'Error: {exc}')
-                else:
-                    surfs.append(surf)
+            # for future in concurrent.futures.as_completed(futures):
+            #     try:
+            #         surf = future.result()
+            #     except Exception as exc:
+            #         logger.error(f'Error: {exc}')
+            #     else:
+            #         surfs.append(surf)
 
-            regular_surfaces = xtgeo.Surfaces(surfs)
-            return regular_surfaces
-
+            # regular_surfaces = xtgeo.Surfaces(surfs)
+            # return regular_surfaces
 
     def _get_regsurf(i):
         logger.info("Start %s", i)
 
-        sfile = TESTSET1A
+        sfile = os.path.join(TMPD, str(i) + ".gri")
         # with open(file, "rb") as fin:
         #     stream = io.BytesIO(fin.read())
 
+        logger.info("File is %s", sfile)
         rf = xtgeo.RegularSurface(sfile)
         logger.info("End %s", i)
         return rf
 
-    _get_files_as_regularsurfaces_thread(1)
+    #
+    # main:
+    #
+    nthread = 10
+    for n in range(nthread):
+        x = xtgeo.RegularSurface()
+        x.to_file(os.path.join(TMPD, str(n) + ".gri"))
+
+    _get_files_as_regularsurfaces_thread(nthread)
