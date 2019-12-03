6a417c67c900a185db1708401a8a23f89866b232
Jan C. Rivenaes <jan@rivenaes.net>
2019-12-03 13:35:18 +0100

    Tuning swig install; redirect output

--- scripts/swig_install.sh
+++ scripts/swig_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+# Try to install swig in cases it is missing on the system
 
 rm -fr _tmp_swiginstall
 mkdir -p _tmp_swiginstall
@@ -14,7 +15,10 @@ wget https://sourceforge.net/projects/swig/files/swig/swig-4.0.1/swig-4.0.1.tar.
 tar xf swig-4.0.1.tar.gz
 pushd swig-4.0.1
 wget "https://ftp.pcre.org/pub/pcre/pcre-8.38.tar.gz"
-./configure --prefix="$INST/_tmp_swig"
+echo "Running configure..."
+./configure --prefix="$INST/_tmp_swig"  > swiginstall.log
 
-make
-make install
+echo "Running make..."
+make >> swiginstall.log
+echo "Running make install..."
+make install >> swiginstall.log
