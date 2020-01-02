1bf5267d751da6760670f27561d504e8eed08a9e
Jan C. Rivenaes <jan@rivenaes.net>
2020-01-02 14:07:51 +0100

    Add if branch when fformat is wrongly spelled, cf #251

--- src/xtgeo/grid3d/_gridprop_export.py
+++ src/xtgeo/grid3d/_gridprop_export.py
@@ -41,9 +41,11 @@ def to_file(self, pfile, fformat="roff", name=None, append=False, dtype=None):
     elif fformat == "bgrdecl":
         export_grdecl(self, pfile, name, append=append, binary=True, dtype=dtype)
 
+    else:
+        raise ValueError("Cannot export, invalid fformat: {}".format(fformat))
+
 
 # Export ascii or binary ROFF format
-
 
 def export_roff(self, pfile, name, append=False, last=True, binary=True):
 
