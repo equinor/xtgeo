798d160af803821bf4f4bca58a7e598342e762d0
Jan C. Rivenaes <jan@rivenaes.net>
2020-01-25 20:57:26 +0100

    Added test for reverse_row_axis for grid with props

--- tests/test_grid3d/test_grid_operations.py
+++ tests/test_grid3d/test_grid_operations.py
@@ -27,31 +27,31 @@ TESTPATH = xtg.testpath
 # =============================================================================
 # Do tests
 # =============================================================================
-EMEGFILE = '../xtgeo-testdata/3dgrids/eme/1/emerald_hetero_grid.roff'
-EMERFILE = '../xtgeo-testdata/3dgrids/eme/1/emerald_hetero_region.roff'
+EMEGFILE = "../xtgeo-testdata/3dgrids/eme/1/emerald_hetero_grid.roff"
+EMERFILE = "../xtgeo-testdata/3dgrids/eme/1/emerald_hetero_region.roff"
 
-EMEGFILE2 = '../xtgeo-testdata/3dgrids/eme/2/emerald_hetero_grid.roff'
-EMEZFILE2 = '../xtgeo-testdata/3dgrids/eme/2/emerald_hetero.roff'
+EMEGFILE2 = "../xtgeo-testdata/3dgrids/eme/2/emerald_hetero_grid.roff"
+EMEZFILE2 = "../xtgeo-testdata/3dgrids/eme/2/emerald_hetero.roff"
 
-DUAL = '../xtgeo-testdata/3dgrids/etc/dual_distorted2.grdecl'
-DUALPROPS = '../xtgeo-testdata/3dgrids/etc/DUAL'
+DUAL = "../xtgeo-testdata/3dgrids/etc/dual_distorted2.grdecl"
+DUALPROPS = "../xtgeo-testdata/3dgrids/etc/DUAL"
 
 
 def test_hybridgrid1():
     """Making a hybridgrid for Emerald case (ROFF and GRDECL"""
 
-    logger.info('Read grid...')
+    logger.info("Read grid...")
     grd = Grid(EMEGFILE)
-    logger.info('Read grid... done, NZ is %s', grd.nlay)
-    grd.to_file(join(TDP, 'test_hybridgrid1_asis.grdecl'), fformat='grdecl')
+    logger.info("Read grid... done, NZ is %s", grd.nlay)
+    grd.to_file(join(TDP, "test_hybridgrid1_asis.grdecl"), fformat="grdecl")
 
-    logger.info('Convert...')
+    logger.info("Convert...")
     nhdiv = 40
     newnlay = grd.nlay * 2 + nhdiv
 
     grd.convert_to_hybrid(nhdiv=nhdiv, toplevel=1700, bottomlevel=1740)
 
-    logger.info('Hybrid grid... done, NLAY is now %s', grd.nlay)
+    logger.info("Hybrid grid... done, NLAY is now %s", grd.nlay)
 
     assert grd.nlay == newnlay, "New NLAY number"
 
@@ -59,22 +59,22 @@ def test_hybridgrid1():
 
     assert dzv.values3d.mean() == pytest.approx(1.395, abs=0.01)
 
-    logger.info('Export...')
-    grd.to_file('TMP/test_hybridgrid1.roff')
+    logger.info("Export...")
+    grd.to_file("TMP/test_hybridgrid1.roff")
 
-    logger.info('Read grid 2...')
-    grd2 = Grid('TMP/test_hybridgrid1_asis.grdecl')
-    logger.info('Read grid... done, NLAY is %s', grd2.nlay)
+    logger.info("Read grid 2...")
+    grd2 = Grid("TMP/test_hybridgrid1_asis.grdecl")
+    logger.info("Read grid... done, NLAY is %s", grd2.nlay)
 
-    logger.info('Convert...')
+    logger.info("Convert...")
     nhdiv = 40
     newnz = grd2.nlay * 2 + nhdiv
 
     grd2.convert_to_hybrid(nhdiv=nhdiv, toplevel=1700, bottomlevel=1740)
 
-    logger.info('Hybrid grid... done, NZ is now %s', grd2.nlay)
+    logger.info("Hybrid grid... done, NZ is now %s", grd2.nlay)
 
-    assert grd2.nlay == newnz, 'New NLAY number'
+    assert grd2.nlay == newnz, "New NLAY number"
 
     dzv = grd2.get_dz()
 
@@ -84,49 +84,51 @@ def test_hybridgrid1():
 def test_hybridgrid2():
     """Making a hybridgrid for Emerald case in region"""
 
-    logger.info('Read grid...')
+    logger.info("Read grid...")
     grd = Grid(EMEGFILE)
-    logger.info('Read grid... done, NLAY is {}'.format(grd.nlay))
+    logger.info("Read grid... done, NLAY is {}".format(grd.nlay))
 
     reg = GridProperty()
-    reg.from_file(EMERFILE, name='REGION')
+    reg.from_file(EMERFILE, name="REGION")
 
     nhdiv = 40
 
-    grd.convert_to_hybrid(nhdiv=nhdiv, toplevel=1650, bottomlevel=1690,
-                          region=reg, region_number=1)
+    grd.convert_to_hybrid(
+        nhdiv=nhdiv, toplevel=1650, bottomlevel=1690, region=reg, region_number=1
+    )
 
-    grd.to_file('TMP/test_hybridgrid2.roff')
+    grd.to_file("TMP/test_hybridgrid2.roff")
 
 
 def test_inactivate_thin_cells():
     """Make hybridgrid for Emerald case in region, and inactive thin cells"""
 
-    logger.info('Read grid...')
+    logger.info("Read grid...")
     grd = Grid(EMEGFILE)
-    logger.info('Read grid... done, NLAY is {}'.format(grd.nlay))
+    logger.info("Read grid... done, NLAY is {}".format(grd.nlay))
 
     reg = GridProperty()
-    reg.from_file(EMERFILE, name='REGION')
+    reg.from_file(EMERFILE, name="REGION")
 
     nhdiv = 40
 
-    grd.convert_to_hybrid(nhdiv=nhdiv, toplevel=1650, bottomlevel=1690,
-                          region=reg, region_number=1)
+    grd.convert_to_hybrid(
+        nhdiv=nhdiv, toplevel=1650, bottomlevel=1690, region=reg, region_number=1
+    )
 
     grd.inactivate_by_dz(0.001)
 
-    grd.to_file('TMP/test_hybridgrid2_inact_thin.roff')
+    grd.to_file("TMP/test_hybridgrid2_inact_thin.roff")
 
 
 def test_refine_vertically():
     """Do a grid refinement vertically."""
 
-    logger.info('Read grid...')
+    logger.info("Read grid...")
 
     grd = Grid(EMEGFILE)
-    logger.info('Read grid... done, NLAY is {}'.format(grd.nlay))
-    logger.info('Subgrids before: %s', grd.get_subgrids())
+    logger.info("Read grid... done, NLAY is {}".format(grd.nlay))
+    logger.info("Subgrids before: %s", grd.get_subgrids())
 
     avg_dz1 = grd.get_dz().values3d.mean()
 
@@ -137,43 +139,42 @@ def test_refine_vertically():
 
     assert avg_dz1 == pytest.approx(3 * avg_dz2, abs=0.0001)
 
-    logger.info('Subgrids after: %s', grd.get_subgrids())
+    logger.info("Subgrids after: %s", grd.get_subgrids())
     grd.inactivate_by_dz(0.001)
 
-    grd.to_file('TMP/test_refined_by_3.roff')
+    grd.to_file("TMP/test_refined_by_3.roff")
 
 
 def test_refine_vertically_per_zone():
     """Do a grid refinement vertically, via a dict per zone."""
 
-    logger.info('Read grid...')
+    logger.info("Read grid...")
 
     grd_orig = Grid(EMEGFILE2)
     grd = grd_orig.copy()
 
-    logger.info('Read grid... done, NLAY is {}'.format(grd.nlay))
-    grd.to_file('TMP/test_refined_by_dict_initial.roff')
+    logger.info("Read grid... done, NLAY is {}".format(grd.nlay))
+    grd.to_file("TMP/test_refined_by_dict_initial.roff")
 
-    logger.info('Subgrids before: %s', grd.get_subgrids())
+    logger.info("Subgrids before: %s", grd.get_subgrids())
 
-    zone = GridProperty(EMEZFILE2, grid=grd, name='Zone')
-    logger.info('Zone values min max: %s %s', zone.values.min(),
-                zone.values.max())
+    zone = GridProperty(EMEZFILE2, grid=grd, name="Zone")
+    logger.info("Zone values min max: %s %s", zone.values.min(), zone.values.max())
 
-    logger.info('Subgrids list: %s', grd.subgrids)
+    logger.info("Subgrids list: %s", grd.subgrids)
 
     refinement = {1: 4, 2: 2}
     grd.refine_vertically(refinement, zoneprop=zone)
 
     grd1s = grd.get_subgrids()
-    logger.info('Subgrids after: %s', grd1s)
+    logger.info("Subgrids after: %s", grd1s)
 
-    grd.to_file('TMP/test_refined_by_dict.roff')
+    grd.to_file("TMP/test_refined_by_dict.roff")
 
     grd = grd_orig.copy()
     grd.refine_vertically(refinement)  # no zoneprop
     grd2s = grd.get_subgrids()
-    logger.info('Subgrids after: %s', grd2s)
+    logger.info("Subgrids after: %s", grd2s)
     assert list(grd1s.values()) == list(grd2s.values())
 
     # grd = grd_orig.copy()
@@ -185,8 +186,12 @@ def test_reverse_row_axis_box():
     """Crop a grid."""
 
     grd = Grid()
-    grd.create_box(origin=(1000, 4000, 300), increment=(100, 100, 2),
-                   dimension=(2, 3, 1), rotation=0)
+    grd.create_box(
+        origin=(1000, 4000, 300),
+        increment=(100, 100, 2),
+        dimension=(2, 3, 1),
+        rotation=0,
+    )
 
     assert grd.ijk_handedness == "left"
     grd.to_file(join(TDP, "reverse_left.grdecl"), fformat="grdecl")
@@ -214,19 +219,25 @@ def test_reverse_row_axis_dual():
 def test_reverse_row_axis_dualprops():
     """Reverse axis for distorted but small grid with props"""
 
-    grd = Grid(DUALPROPS, fformat="eclipserun")
+    grd = Grid(DUALPROPS, fformat="eclipserun", initprops=["PORO", "PORV"])
 
-    print("XXX", grd.gridprops.props)
+    poro = grd.gridprops.props[0]
+    logger.info(grd.gridprops.describe())
+    porowas = poro.copy()
+    tsetup.assert_almostequal(poro.values[1, 0, 0], 0.17777, 0.01)
+    assert grd.ijk_handedness == "left"
 
-    # assert grd.ijk_handedness == "left"
-    # grd.to_file(join(TDP, "dual_left.grdecl"), fformat="grdecl")
-    # cellcorners1 = grd.get_xyz_cell_corners((5, 1, 1))
-    # grd.reverse_row_axis()
-    # assert grd.ijk_handedness == "right"
-    # grd.to_file(join(TDP, "dual_right.grdecl"), fformat="grdecl")
-    # cellcorners2 = grd.get_xyz_cell_corners((5, 3, 1))
+    grd.reverse_row_axis()
+    tsetup.assert_almostequal(poro.values[1, 2, 0], 0.17777, 0.01)
+    assert poro.values[1, 2, 0] == porowas.values[1, 0, 0]
 
-    # assert cellcorners1[7] == cellcorners2[1]
+    grd.reverse_row_axis()
+    assert poro.values[1, 0, 0] == porowas.values[1, 0, 0]
+    assert grd.ijk_handedness == "left"
+
+    grd.reverse_row_axis(ijk_handedness="left")  # ie do nothing in this case
+    assert poro.values[1, 0, 0] == porowas.values[1, 0, 0]
+    assert grd.ijk_handedness == "left"
 
 
 def test_reverse_row_axis_eme():
@@ -252,28 +263,29 @@ def test_copy_grid():
     grd = Grid(EMEGFILE2)
     grd2 = grd.copy()
 
-    grd.to_file(join('TMP', 'gcp1.roff'))
-    grd2.to_file(join('TMP', 'gcp2.roff'))
-    assert cmp(join('TMP', 'gcp1.roff'), join('TMP', 'gcp2.roff')) is True
+    grd.to_file(join("TMP", "gcp1.roff"))
+    grd2.to_file(join("TMP", "gcp2.roff"))
+    assert cmp(join("TMP", "gcp1.roff"), join("TMP", "gcp2.roff")) is True
 
 
 def test_crop_grid():
     """Crop a grid."""
 
-    logger.info('Read grid...')
+    logger.info("Read grid...")
 
     grd = Grid(EMEGFILE2)
-    zprop = GridProperty(EMEZFILE2, name='Zone', grid=grd)
+    zprop = GridProperty(EMEZFILE2, name="Zone", grid=grd)
 
-    logger.info('Read grid... done, NLAY is {}'.format(grd.nlay))
-    logger.info('Read grid...NCOL, NROW, NLAY is {} {} {}'
-                .format(grd.ncol, grd.nrow, grd.nlay))
+    logger.info("Read grid... done, NLAY is {}".format(grd.nlay))
+    logger.info(
+        "Read grid...NCOL, NROW, NLAY is {} {} {}".format(grd.ncol, grd.nrow, grd.nlay)
+    )
 
     grd.crop((30, 60), (20, 40), (1, 46), props=[zprop])
 
-    grd.to_file(join('TMP', 'grid_cropped.roff'))
+    grd.to_file(join("TMP", "grid_cropped.roff"))
 
-    grd2 = Grid(join('TMP', 'grid_cropped.roff'))
+    grd2 = Grid(join("TMP", "grid_cropped.roff"))
 
     assert grd2.ncol == 31
 
@@ -281,11 +293,11 @@ def test_crop_grid():
 def test_crop_grid_after_copy():
     """Copy a grid, then crop and check number of active cells."""
 
-    logger.info('Read grid...')
+    logger.info("Read grid...")
 
     grd = Grid(EMEGFILE2)
     grd.describe()
-    zprop = GridProperty(EMEZFILE2, name='Zone', grid=grd)
+    zprop = GridProperty(EMEZFILE2, name="Zone", grid=grd)
     grd.describe(details=True)
 
     logger.info(grd.dimensions)
@@ -293,13 +305,13 @@ def test_crop_grid_after_copy():
     grd2 = grd.copy()
     grd2.describe(details=True)
 
-    logger.info('GRD2 props: %s', grd2.props)
+    logger.info("GRD2 props: %s", grd2.props)
     assert grd.propnames == grd2.propnames
 
-    logger.info('GRD2 number of active cells: %s', grd2.nactive)
+    logger.info("GRD2 number of active cells: %s", grd2.nactive)
     act = grd.get_actnum()
     logger.info(act.values.shape)
-    logger.info('ZPROP: %s', zprop.values.shape)
+    logger.info("ZPROP: %s", zprop.values.shape)
 
     grd2.crop((1, 30), (40, 80), (23, 46))
 
@@ -309,7 +321,7 @@ def test_crop_grid_after_copy():
 def test_reduce_to_one_layer():
     """Crop a grid."""
 
-    logger.info('Read grid...')
+    logger.info("Read grid...")
 
     grd1 = Grid(EMEGFILE2)
     geom1v1 = grd1.get_geometrics(allcells=True, cellcenter=True)
