b8dc03be93b2dd8b3c35932c107b9bc54bf87a4b
Jan C. Rivenaes <jan@rivenaes.net>
2020-03-31 16:38:27 +0200

    Extended tests for zone grid vs well mismatch

--- tests/test_grid3d/test_grid_vs_well.py
+++ tests/test_grid3d/test_grid_vs_well.py
@@ -2,6 +2,10 @@
 from __future__ import division, absolute_import
 from __future__ import print_function
 
+from os.path import join
+
+import pytest
+
 from xtgeo.grid3d import Grid
 from xtgeo.well import Well
 from xtgeo.grid3d import GridProperty
@@ -13,7 +17,7 @@ logger = xtg.basiclogger(__name__)
 if not xtg.testsetup():
     raise SystemExit
 
-TDMP = xtg.tmpdir
+TMPD = xtg.tmpdir
 
 # =============================================================================
 # Do tests
@@ -28,6 +32,28 @@ WELL4 = "../xtgeo-testdata/wells/reek/1/OP_4.w"
 WELL5 = "../xtgeo-testdata/wells/reek/1/OP_5.w"
 WELL6 = "../xtgeo-testdata/wells/reek/1/WI_1.w"
 WELL7 = "../xtgeo-testdata/wells/reek/1/WI_3.w"
+
+PWELL1 = "../xtgeo-testdata/wells/reek/1/OP1_perf.w"
+
+MATCHD1 = {
+    "WI_1": 75,
+    "WI_3": 75,
+    "OP_4": 78,
+    "OP_5": 78,
+    "OP_1": 80,
+    "OP_2": 77,
+    "OP_3": 77,
+}
+
+MATCHD2 = {
+    "WI_1": 65,
+    "WI_3": 40,
+    "OP_4": 71,
+    "OP_5": 69,
+    "OP_1": 71,
+    "OP_2": 65,
+    "OP_3": 70,
+}
 
 # A problem here is that the OP wells has very few samples, which
 # makes a assumed match of 100% (since only one point)
@@ -53,10 +79,6 @@ def test_report_zlog_mismatch():
 
     wells = [w1, w2, w3, w4, w5, w6, w7]
 
-    resultd = {}
-    # matchd = {'WI_1': 69, 'WI_3': 70, 'OP_4': 74, 'OP_5': 75, 'OP_1': 75,
-    #           'OP_2': 74, 'OP_3': 70}
-
     for wll in wells:
         response = g1.report_zone_mismatch(
             well=wll,
@@ -66,11 +88,89 @@ def test_report_zlog_mismatch():
             depthrange=[1300, 9999],
         )
 
-        if response is not None:
-            print(response)
-            # match = int(float("{0:.4f}".format(response[0])))
-            # logger.info(match)
-            # resultd[wll.wellname] = match
+        match = int(float("{0:.4f}".format(response[0])))
+        logger.info("Match for %s is %s", wll.wellname, match)
+        assert match == MATCHD1[wll.name]
+
+        # check also with resultformat=2
+        res = g1.report_zone_mismatch(
+            well=wll,
+            zonelogname="Zonelog",
+            zoneprop=zo,
+            zonelogrange=(1, 3),
+            depthrange=[1300, 9999],
+            resultformat=2,
+        )
+
+        match = int(float("{0:.4f}".format(res["MATCH2"])))
+        logger.info("Match for %s is %s", wll.wellname, match)
+        assert match == MATCHD2[wll.name]
 
 
-#    assert resultd == matchd
+def test_report_zlog_mismatch_resultformat3():
+    """Report zone log mismatch grid and well, export updated wellsegment"""
+    g1 = Grid()
+    g1.from_file(GRIDFILE)
+
+    zo = GridProperty()
+    zo.from_file(ZONEFILE, name="Zone")
+
+    w1 = Well(WELL1)
+
+    res = g1.report_zone_mismatch(
+        well=w1,
+        zonelogname="Zonelog",
+        zoneprop=zo,
+        zonelogrange=(1, 3),
+        depthrange=[1300, 9999],
+        resultformat=3,
+    )
+    mywell = res["WELLINTV"]
+    logger.info("\n%s", mywell.dataframe.to_string())
+    mywell.to_file(join(TMPD, "w1_zlog_report.rmswell"))
+
+
+def test_report_zlog_mismatch_perflog():
+    """Report zone log mismatch grid and well filter on PERF"""
+    g1 = Grid()
+    g1.from_file(GRIDFILE)
+
+    zo = GridProperty()
+    zo.from_file(ZONEFILE, name="Zone")
+
+    w1 = Well(PWELL1)
+
+    w1.dataframe.to_csv("TMP/testw1.csv")
+
+    res = g1.report_zone_mismatch(
+        well=w1,
+        zonelogname="Zonelog",
+        zoneprop=zo,
+        zonelogrange=(1, 3),
+        depthrange=[1580, 9999],
+        perflogname="PERF",
+        resultformat=2,
+    )
+    mywell = res["WELLINTV"]
+    logger.info("\n%s", mywell.dataframe.to_string())
+    mywell.to_file(join(TMPD, "w1_perf_report.rmswell"))
+
+    assert res["MATCH2"] == pytest.approx(82.14, 0.001)
+    assert res["TCOUNT2"] == 56
+    assert res["MCOUNT2"] == 46
+
+    w1 = Well(WELL1)
+
+    # well is missing perflog; hence result shall be None
+    res = g1.report_zone_mismatch(
+        well=w1,
+        zonelogname="Zonelog",
+        zoneprop=zo,
+        zonelogrange=(1, 3),
+        depthrange=[1580, 9999],
+        perflogname="PERF",
+        resultformat=2,
+    )
+
+    # ask for perflogname but no such present
+    assert res is None
