53cdaa44d5dd9d237a1eb52c22b603f166591678
Jan C. Rivenæs <jan@rivenaes.net>
2020-04-16 21:45:48 +0200

    Fix pylint warnings

--- tests/test_well/test_well.py
+++ tests/test_well/test_well.py
@@ -28,26 +28,24 @@ TESTPATH = xtg.testpath
 # Do tests
 # =========================================================================
 
-WFILE = join(TESTPATH, 'wells/reek/1/OP_1.w')
-WFILE_HOLES = join(TESTPATH, 'wells/reek/1/OP_1_zholes.w')
-WFILES = join(TESTPATH, 'wells/reek/1/*')
+WFILE = join(TESTPATH, "wells/reek/1/OP_1.w")
+WFILE_HOLES = join(TESTPATH, "wells/reek/1/OP_1_zholes.w")
+WFILES = join(TESTPATH, "wells/reek/1/*")
 
-WELL1 = join(TESTPATH, 'wells/battle/1/WELL09.rmswell')
-WELL2 = join(TESTPATH, 'wells/battle/1/WELL36.rmswell')
-WELL3 = join(TESTPATH, 'wells/battle/1/WELL10.rmswell')
+WELL1 = join(TESTPATH, "wells/battle/1/WELL09.rmswell")
+WELL2 = join(TESTPATH, "wells/battle/1/WELL36.rmswell")
+WELL3 = join(TESTPATH, "wells/battle/1/WELL10.rmswell")
 
 
-@pytest.fixture()
-def loadwell1():
+@pytest.fixture(name="loadwell1")
+def fixture_loadwell1():
     """Fixture for loading a well (pytest setup)"""
-    logger.info('Load well 1: {}'.format(WFILE))
     return Well(WFILE)
 
 
-@pytest.fixture()
-def loadwell3():
+@pytest.fixture(name="loadwell3")
+def fixture_loadwell3():
     """Fixture for loading a well (pytest setup)"""
-    logger.info('Load well 3')
     return Well(WELL3)
 
 
@@ -56,18 +54,18 @@ def test_import(loadwell1):
 
     mywell = loadwell1
 
-    logger.debug('True well name:', mywell.truewellname)
-    tsetup.assert_equal(mywell.xpos, 461809.59, 'XPOS')
-    tsetup.assert_equal(mywell.ypos, 5932990.36, 'YPOS')
-    tsetup.assert_equal(mywell.wellname, 'OP_1', 'WNAME')
+    logger.debug("True well name: %s", mywell.truewellname)
+    tsetup.assert_equal(mywell.xpos, 461809.59, "XPOS")
+    tsetup.assert_equal(mywell.ypos, 5932990.36, "YPOS")
+    tsetup.assert_equal(mywell.wellname, "OP_1", "WNAME")
 
-    logger.info(mywell.get_logtype('Zonelog'))
-    logger.info(mywell.get_logrecord('Zonelog'))
+    logger.info(mywell.get_logtype("Zonelog"))
+    logger.info(mywell.get_logrecord("Zonelog"))
     logger.info(mywell.lognames_all)
     logger.info(mywell.dataframe)
 
     # logger.info the numpy string of Poro...
-    logger.info(type(mywell.dataframe['Poro'].values))
+    logger.info(type(mywell.dataframe["Poro"].values))
 
 
 def test_import_long_well(loadwell3):
@@ -75,12 +73,12 @@ def test_import_long_well(loadwell3):
 
     mywell = loadwell3
 
-    logger.debug('True well name:', mywell.truewellname)
+    logger.debug("True well name: %s", mywell.truewellname)
 
     mywell.geometrics()
     dfr = mywell.dataframe
 
-    tsetup.assert_almostequal(dfr['Q_AZI'][27], 91.856158, 0.0001)
+    tsetup.assert_almostequal(dfr["Q_AZI"][27], 91.856158, 0.0001)
 
 
 def test_import_well_selected_logs():
@@ -120,31 +118,31 @@ def test_change_a_lot_of_stuff(loadwell1):
 
     mywell = loadwell1
 
-    assert mywell.get_logtype('Poro') == 'CONT'
-    assert mywell.get_logrecord('Poro') is None
+    assert mywell.get_logtype("Poro") == "CONT"
+    assert mywell.get_logrecord("Poro") is None
 
     with pytest.raises(ValueError) as vinfo:
-        mywell.set_logrecord('Poro', {0: 'null'})
-    assert 'Cannot set a log record for a continuous log' in str(vinfo.value)
+        mywell.set_logrecord("Poro", {0: "null"})
+    assert "Cannot set a log record for a continuous log" in str(vinfo.value)
 
-    assert mywell.name == 'OP_1'
-    mywell.name = 'OP_1_EDITED'
-    assert mywell.name == 'OP_1_EDITED'
+    assert mywell.name == "OP_1"
+    mywell.name = "OP_1_EDITED"
+    assert mywell.name == "OP_1_EDITED"
 
-    mywell.rename_log('Poro', 'PORO')
-    assert 'PORO' in mywell.lognames
-
-    with pytest.raises(ValueError) as vinfo:
-        mywell.rename_log('PoroXX', 'Perm')
-    assert 'Input log does not exist' in str(vinfo.value)
+    mywell.rename_log("Poro", "PORO")
+    assert "PORO" in mywell.lognames
 
     with pytest.raises(ValueError) as vinfo:
-        mywell.rename_log('PORO', 'Perm')
-    assert 'New log name exists already' in str(vinfo.value)
+        mywell.rename_log("PoroXX", "Perm")
+    assert "Input log does not exist" in str(vinfo.value)
 
-    frec1 = mywell.get_logrecord('Facies')
-    mywell.rename_log('Facies', 'FACIES')
-    frec2 = mywell.get_logrecord('FACIES')
+    with pytest.raises(ValueError) as vinfo:
+        mywell.rename_log("PORO", "Perm")
+    assert "New log name exists already" in str(vinfo.value)
+
+    frec1 = mywell.get_logrecord("Facies")
+    mywell.rename_log("Facies", "FACIES")
+    frec2 = mywell.get_logrecord("FACIES")
 
     assert sorted(frec1) == sorted(frec2)
 
@@ -155,14 +153,12 @@ def test_import_export_many():
     logger.debug(WFILES)
 
     for filename in sorted(glob.glob(WFILES)):
-        logger.info('Importing ' + filename)
         mywell = Well(filename)
         logger.info(mywell.nrow)
         logger.info(mywell.ncol)
         logger.info(mywell.lognames)
 
-        wname = join(TMPD, mywell.xwellname + '.w')
-        logger.info('Exporting ' + wname)
+        wname = join(TMPD, mywell.xwellname + ".w")
         mywell.to_file(wname)
 
 
@@ -171,15 +167,15 @@ def test_shortwellname():
 
     mywell = Well()
 
-    mywell._wname = '31/2-A-14 2H'
+    mywell._wname = "31/2-A-14 2H"
     short = mywell.shortwellname
 
-    assert short == 'A-142H'
+    assert short == "A-142H"
 
-    mywell._wname = '6412_2-A-14_2H'
+    mywell._wname = "6412_2-A-14_2H"
     short = mywell.shortwellname
 
-    assert short == 'A-142H'
+    assert short == "A-142H"
 
 
 # def test_import_as_rms_export_as_hdf5_many():
@@ -205,49 +201,49 @@ def test_get_carr(loadwell1):
 
     mywell = loadwell1
 
-    dummy = mywell.get_carray('NOSUCH')
+    dummy = mywell.get_carray("NOSUCH")
 
-    tsetup.assert_equal(dummy, None, 'Wrong log name')
+    tsetup.assert_equal(dummy, None, "Wrong log name")
 
-    cref = mywell.get_carray('X_UTME')
-
-    xref = str(cref)
-    swig = False
-    if 'Swig' in xref and 'double' in xref:
-        swig = True
-
-    tsetup.assert_equal(swig, True, 'carray from log name, double')
-
-    cref = mywell.get_carray('Zonelog')
+    cref = mywell.get_carray("X_UTME")
 
     xref = str(cref)
     swig = False
-    if 'Swig' in xref and 'int' in xref:
+    if "Swig" in xref and "double" in xref:
         swig = True
 
-    tsetup.assert_equal(swig, True, 'carray from log name, int')
+    tsetup.assert_equal(swig, True, "carray from log name, double")
+
+    cref = mywell.get_carray("Zonelog")
+
+    xref = str(cref)
+    swig = False
+    if "Swig" in xref and "int" in xref:
+        swig = True
+
+    tsetup.assert_equal(swig, True, "carray from log name, int")
 
 
 def test_create_and_delete_logs(loadwell3):
 
     mywell = loadwell3
 
-    status = mywell.create_log('NEWLOG')
+    status = mywell.create_log("NEWLOG")
     assert status is True
 
-    status = mywell.create_log('NEWLOG', force=False)
+    status = mywell.create_log("NEWLOG", force=False)
     assert status is False
 
-    status = mywell.create_log('NEWLOG', force=True, value=200)
+    status = mywell.create_log("NEWLOG", force=True, value=200)
     assert status is True
     assert mywell.dataframe.NEWLOG.mean() == 200.0
 
-    ndeleted = mywell.delete_log('NEWLOG')
+    ndeleted = mywell.delete_log("NEWLOG")
 
     assert ndeleted == 1
-    status = mywell.create_log('NEWLOG', force=True, value=200)
+    status = mywell.create_log("NEWLOG", force=True, value=200)
 
-    ndeleted = mywell.delete_log(['NEWLOG', 'GR'])
+    ndeleted = mywell.delete_log(["NEWLOG", "GR"])
     assert ndeleted == 2
 
 
@@ -264,11 +260,11 @@ def test_make_zqual_log(loadwell3):
     """Make a zonelog FLAG quality log"""
 
     mywell = loadwell3
-    mywell.zonelogname = 'ZONELOG'
+    mywell.zonelogname = "ZONELOG"
 
-    logger.debug('True well name:', mywell.truewellname)
+    logger.debug("True well name: %s", mywell.truewellname)
 
-    mywell.make_zone_qual_log('manamana')
+    mywell.make_zone_qual_log("manamana")
 
     with pd.option_context("display.max_rows", 1000):
         print(mywell.dataframe)
@@ -280,15 +276,15 @@ def test_rescale_well(loadwell1):
     mywell = loadwell1
 
     df1 = mywell.dataframe.copy()
-    df1 = df1[(df1['Zonelog'] == 1)]
+    df1 = df1[(df1["Zonelog"] == 1)]
 
     mywell.rescale(delta=0.2)
 
     df2 = mywell.dataframe.copy()
-    df2 = df2[(df2['Zonelog'] == 1)]
+    df2 = df2[(df2["Zonelog"] == 1)]
 
-    tsetup.assert_almostequal(df1['Perm'].mean(), df2['Perm'].mean(), 20.0)
-    tsetup.assert_almostequal(df1['Poro'].mean(), df2['Poro'].mean(), 0.001)
+    tsetup.assert_almostequal(df1["Perm"].mean(), df2["Perm"].mean(), 20.0)
+    tsetup.assert_almostequal(df1["Poro"].mean(), df2["Poro"].mean(), 0.001)
 
 
 def test_rescale_well_tvdrange():
@@ -320,7 +316,7 @@ def test_rescale_well_tvdrange_coarsen_upper():
     tsetup.assert_almostequal(mywell.dataframe.iat[10, 3], 365.8254, 0.1)
 
 
-def test_fence(loadwell1):
+def test_fence():
     """Return a resampled fence."""
 
     mywell = Well(WFILE)
@@ -329,30 +325,29 @@ def test_fence(loadwell1):
     logger.debug(pline)
 
 
-def test_fence_as_polygons(loadwell1):
+def test_fence_as_polygons():
     """Return a resampled fence as Polygons."""
 
     mywell = Well(WFILE)
-    pline = mywell.get_fence_polyline(nextend=3, tvdmin=1000,
-                                      asnumpy=False)
+    pline = mywell.get_fence_polyline(nextend=3, tvdmin=1000, asnumpy=False)
 
     assert isinstance(pline, Polygons)
     dfr = pline.dataframe
     print(dfr)
-    tsetup.assert_almostequal(dfr['X_UTME'][5], 462569.00, 2.0)
+    tsetup.assert_almostequal(dfr["X_UTME"][5], 462569.00, 2.0)
 
 
 def test_get_zonation_points():
     """Get zonations points (zone tops)"""
 
-    mywell = Well(WFILE, zonelogname='Zonelog')
+    mywell = Well(WFILE, zonelogname="Zonelog")
     mywell.get_zonation_points()
 
 
 def test_get_zone_interval():
     """Get zonations points (zone tops)"""
 
-    mywell = Well(WFILE, zonelogname='Zonelog')
+    mywell = Well(WFILE, zonelogname="Zonelog")
     line = mywell.get_zone_interval(3)
 
     print(line)
@@ -377,10 +372,10 @@ def test_remove_parallel_parts():
 def test_get_zonation_holes():
     """get a report of holes in the zonation, some samples with -999 """
 
-    mywell = Well(WFILE_HOLES, zonelogname='Zonelog')
+    mywell = Well(WFILE_HOLES, zonelogname="Zonelog")
     report = mywell.report_zonation_holes()
 
-    logger.info('\n{}'.format(report))
+    logger.info("\n%s", report)
 
     tsetup.assert_equal(report.iat[0, 0], 4193)  # first value for INDEX
     tsetup.assert_equal(report.iat[1, 3], 1609.5800)  # second value for Z
