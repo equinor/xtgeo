2e423748d1cfcf109d3b1bb5888f115af28f5afe
Jan C. Rivenæs <jan@rivenaes.net>
2020-09-16 11:29:34 +0200

    Roff export binary/ascii xtgformat=2

--- src/xtgeo/grid3d/_grid_export.py
+++ src/xtgeo/grid3d/_grid_export.py
@@ -15,7 +15,7 @@ def export_roff(self, gfile, option):
     if self._xtgformat == 1:
         _export_roff_v1(self, gfile, option)
     else:
-        _export_roff_v2(self, gfile)
+        _export_roff_v2(self, gfile, option)
 
 
 def _export_roff_v1(self, gfile, option):
@@ -62,36 +62,38 @@ def _export_roff_v1(self, gfile, option):
     _cxtgeo.grd3d_export_roff_end(option, gfile.name)
 
 
-def _export_roff_v2(self, gfile):
-    """Export grid to ROFF format (binary) _xtgformat=2"""
+def _export_roff_v2(self, gfile, ascii_fmt):
+    """Export grid to ROFF format (binary/ascii) _xtgformat=2"""
 
     self._xtgformat2()
 
     gfile = xtgeo._XTGeoFile(gfile, mode="wb")
     gfile.check_folder(raiseerror=OSError)
+    cfhandle = gfile.get_cfhandle()
 
-    logger.debug("Export to ROFF...")
+    logger.debug("Export to ROFF... ascii_fmt = %s", ascii_fmt)
 
-    nsubs = 0
-    if self.subgrids is None:
-        logger.debug("Create a pointer for subgrd_v ...")
-        subgrd_v = _cxtgeo.new_intpointer()
-    else:
-        nsubs = len(self.subgrids)
-        subgrd_v = _cxtgeo.new_intarray(nsubs)
-        for inum, (sname, sarray) in enumerate(self.subgrids.items()):
-            logger.info("INUM SUBGRID: %s %s", inum, sname)
-            _cxtgeo.intarray_setitem(subgrd_v, inum, len(sarray))
+    # nsubs = 0
+    # if self.subgrids is None:
+    #     logger.debug("Create a pointer for subgrd_v ...")
+    #     subgrd_v = _cxtgeo.new_intpointer()
+    # else:
+    #     nsubs = len(self.subgrids)
+    #     subgrd_v = _cxtgeo.new_intarray(nsubs)
+    #     for inum, (sname, sarray) in enumerate(self.subgrids.items()):
+    #         logger.info("INUM SUBGRID: %s %s", inum, sname)
+    #         _cxtgeo.intarray_setitem(subgrd_v, inum, len(sarray))
 
     # get the geometrics list to find the xshift, etc
     # gx = self.get_geometrics()
 
     _cxtgeo.grdcp3d_export_roff_bin_start_end(
-        0, self.ncol, self.nrow, self.nlay, gfile.get_cfhandle()
+        0, ascii_fmt, "grid", self.ncol, self.nrow, self.nlay, cfhandle
     )
 
     # TODO: subgrids
     _cxtgeo.grdcp3d_export_roff_grid(
+        ascii_fmt,
         self._ncol,
         self._nrow,
         self._nlay,
@@ -101,15 +103,16 @@ def _export_roff_v2(self, gfile):
         self._coordsv,
         self._zcornsv,
         self._actnumsv,
-        gfile.get_cfhandle(),
+        cfhandle,
     )
 
-    # TODO: export assosiated properties
+    # # TODO: export assosiated properties
 
     # end tag
     _cxtgeo.grdcp3d_export_roff_bin_start_end(
-        1, self.ncol, self.nrow, self.nlay, gfile.get_cfhandle()
+        1, ascii_fmt, "xxxx", self.ncol, self.nrow, self.nlay, cfhandle
     )
+    gfile.cfclose()
 
 
 def export_grdecl(self, gfile, mode):
