1e54f8b9764456a017674a03e04ee88a362e0f05
Jan C. Rivenæs <jan@rivenaes.net>
2020-09-22 15:41:06 +0200

    Add example on map gridding a 3D grid property

---
+++ examples/grid3d_prop_as_map.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Make a regularmap from a property at a given K layer
+By JRIV
+"""
+
+import xtgeo
+
+GNAMEROOT = "../../xtgeo-testdata/3dgrids/reek/REEK"
+
+
+def make_map():
+    """Make a map of poro or perm in lowermost K layer of the grid"""
+
+    # read grid
+    grd = xtgeo.grid_from_file(GNAMEROOT + ".EGRID")
+
+    _ = xtgeo.gridproperty_from_file(GNAMEROOT + ".INIT", name="PORO", grid=grd)
+
+    df = grd.dataframe()
+
+    # make a map from the grid geometry to be used as a template
+
+    surf = xtgeo.RegularSurface()
+    surf.from_grid3d(grd)
+
+    # get only bottom layer:
+    lastlayer = df["KZ"].max()
+    df = df[df["KZ"] == lastlayer].reset_index()
+
+    # prepare as input to a Points dataframe (3 columns X Y Z)
+    df = df[["X_UTME", "Y_UTMN", "PORO"]].copy()
+
+    points = xtgeo.Points()
+    points.zname = "PORO"
+    points.dataframe = df
+
+    # do gridding:
+    surf.gridding(points)
+
+    # optional plot
+    surf.quickplot()
+
+
+if __name__ == "__main__":
+
+    make_map()
