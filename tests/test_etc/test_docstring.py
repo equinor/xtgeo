48bae3bd37b007631a3bb2ee1b9fcc7883b35dec
Øyvind Eide <eide.oyvind87@gmail.com>
2021-03-04 13:46:12 +0100

    CLN: Remove duplicate methods
    Several methods are calling the parent method without changes, so
    changed to just using the parent method directly.

---
+++ tests/test_etc/test_docstring.py
@@ -0,0 +1,41 @@
+from xtgeo.common.sys import inherit_docstring
+
+
+class SuperClass:
+    def my_function(self):
+        """I have a doc string"""
+
+
+def test_docstring():
+
+    class SubClass(SuperClass):
+        @inherit_docstring(inherit_from=SuperClass.my_function)
+        def my_function(self):
+            pass
+
+    assert SubClass.my_function.__doc__ == "I have a doc string"
+
+
+def test_new_docstring():
+
+    class SubClass(SuperClass):
+        @inherit_docstring(inherit_from=SuperClass.my_function)
+        def my_function(self):
+            """I have a different doc string"""
+            pass
+
+    assert SubClass.my_function.__doc__ == "I have a different doc string"
+
+
+def test_no_super_docstring():
+    class SuperClass:
+        def my_function(self):
+            pass
+
+    class SubClass(SuperClass):
+        @inherit_docstring(inherit_from=SuperClass.my_function)
+        def my_function(self):
+            """I have a doc string"""
+            pass
+
+    assert SubClass.my_function.__doc__ == "I have a doc string"
