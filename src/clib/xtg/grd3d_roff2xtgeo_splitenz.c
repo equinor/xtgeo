e67dfa88481688815f55f13133f9b27ef49bc193
Eivind Jahren <eivind.jahren@webstep.no>
2021-05-19 12:19:27 +0200

    Run clang-format

--- src/clib/xtg/grd3d_roff2xtgeo_splitenz.c
+++ src/clib/xtg/grd3d_roff2xtgeo_splitenz.c
@@ -34,63 +34,62 @@
 #include "logger.h"
 
 int
-grd3d_roff2xtgeo_splitenz(
-                       int nz,
-                       float zoffset,
-                       float zscale,
-                       char *splitenz,
-                       long nsplitenz,
-                       float *zdata,
-                       long nzdata,
-                       float *zcornsv,
-                       long nzcorn)
+grd3d_roff2xtgeo_splitenz(int nz,
+                          float zoffset,
+                          float zscale,
+                          char *splitenz,
+                          long nsplitenz,
+                          float *zdata,
+                          long nzdata,
+                          float *zcornsv,
+                          long nzcorn)
 
 {
     long num_row = 4 * nz;
     float pillar[num_row];
-    if(nzcorn % num_row != 0){
+    if (nzcorn % num_row != 0) {
         return -4;
     }
-    if(nsplitenz != nzcorn / 4){
+    if (nsplitenz != nzcorn / 4) {
         return -2;
     }
     size_t it_zdata = 0, it_splitenz = 0, it_zcorn = 0;
-    while(it_zcorn < nzcorn){
-        for(size_t it_pillar = 0; it_pillar < num_row;){
+    while (it_zcorn < nzcorn) {
+        for (size_t it_pillar = 0; it_pillar < num_row;) {
             char split = splitenz[it_splitenz++];
-            if(split == 1){
-                if(it_zdata >= nzdata){
+            if (split == 1) {
+                if (it_zdata >= nzdata) {
                     return -3;
                 }
                 float val = (zdata[it_zdata++] + zoffset) * zscale;
-                for(int n = 0; n < 4; n++){
+                for (int n = 0; n < 4; n++) {
                     pillar[it_pillar++] = val;
                 }
-            } else if(split == 4){
-                    if(it_zdata+3 >= nzdata){
-                        return -3;
-                    }
-                    pillar[it_pillar+3] = (zdata[it_zdata++] + zoffset) * zscale;
-                    pillar[it_pillar+2] = (zdata[it_zdata++] + zoffset) * zscale;
-                    pillar[it_pillar+1] = (zdata[it_zdata++] + zoffset) * zscale;
-                    pillar[it_pillar]   = (zdata[it_zdata++] + zoffset) * zscale;
-                    it_pillar += 4;
+            } else if (split == 4) {
+                if (it_zdata + 3 >= nzdata) {
+                    return -3;
+                }
+                pillar[it_pillar + 3] = (zdata[it_zdata++] + zoffset) * zscale;
+                pillar[it_pillar + 2] = (zdata[it_zdata++] + zoffset) * zscale;
+                pillar[it_pillar + 1] = (zdata[it_zdata++] + zoffset) * zscale;
+                pillar[it_pillar] = (zdata[it_zdata++] + zoffset) * zscale;
+                it_pillar += 4;
             } else {
                 return -1;
             }
         }
-        for(size_t it_pillar = num_row; it_pillar >= 1;){
+        for (size_t it_pillar = num_row; it_pillar >= 1;) {
             zcornsv[it_zcorn++] = pillar[--it_pillar];
         }
     }
 
-    if(it_splitenz != nsplitenz){
+    if (it_splitenz != nsplitenz) {
         return -2;
     }
-    if(it_zdata != nzdata){
+    if (it_zdata != nzdata) {
         return -3;
     }
-    if(it_zcorn != nzcorn){
+    if (it_zcorn != nzcorn) {
         return -4;
     }
 
