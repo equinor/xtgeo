236096d6d7b90da2059991a58afab755e5a288cf
Jan C. Rivenæs <jan@rivenaes.net>
2022-10-10 15:46:00 +0200

    BUG,TST: fix pandas version test for line_terminator kw

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,8 +9,8 @@ import pytest
 class Helpers:
     @staticmethod
     def df2csv(dataframe, index=True):
-        """Combat Pandas change 1.3 -> 1.4; avoid FutureWarning on line_terminator."""
-        if LooseVersion(pd.__version__) < LooseVersion("1.4"):
+        """Combat Pandas change 1.4 -> 1.5; avoid FutureWarning on line_terminator."""
+        if LooseVersion(pd.__version__) < LooseVersion("1.5"):
             return dataframe.to_csv(line_terminator="\n", index=index)
         else:
             return dataframe.to_csv(lineterminator="\n", index=index)
