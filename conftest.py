dcaa2818f0cdc73c941b60b6a6ea316c98fcbc6c
mferrera <matt.ferrera@gmail.com>
2023-10-24 11:36:43 +0200

    TST: Use pytest-xdist
    hypothesis tests become flaky with pytest-xdist due to timing issues
    when initializing values to draw from. This change removes the separate
    hypothesis profiles for ci and disables hypothesis deadlines by default.
    
    The ci/ci-fast profiles are removed because the timing difference between
    them are barely different when the tests are parallelized.

--- conftest.py
+++ conftest.py
@@ -5,14 +5,11 @@ import pytest
 from hypothesis import HealthCheck, settings
 
 settings.register_profile(
-    "ci", max_examples=250, deadline=None, suppress_health_check=[HealthCheck.too_slow]
-)
-settings.register_profile(
-    "ci-fast",
-    max_examples=10,
+    "no_timeouts",
     deadline=None,
     suppress_health_check=[HealthCheck.too_slow],
 )
+settings.load_profile("no_timeouts")
 
 
 @pytest.fixture
