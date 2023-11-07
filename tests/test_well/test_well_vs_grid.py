b0aa97765538c087f750053b8dd7b278cbaada38
Jan C. Rivenæs <jan@rivenaes.net>
2023-11-07 13:06:44 +0100

    BUG: fix logrecord codes well.get_gridproperties()

--- tests/test_well/test_well_vs_grid.py
+++ tests/test_well/test_well_vs_grid.py
@@ -85,3 +85,22 @@ def test_well_get_gridprops(tmpdir, loadwell1, loadgrid1, loadporo1):
     assert mywell.dataframe.iloc[4775]["PORO_model"] == pytest.approx(0.2741, abs=0.001)
     assert mywell.dataframe.iloc[4775]["ACTNUM_model"] == 1
     assert mywell.isdiscrete("ACTNUM_model") is True
+
+
+def test_well_gridprops_zone(loadwell1):
+    """Test getting logrecords from discrete gridzones"""
+    grid = xtgeo.grid_from_file("../xtgeo-testdata/3dgrids/reek/reek_sim_grid.roff")
+    gridzones = xtgeo.gridproperty_from_file(
+        "../xtgeo-testdata/3dgrids/reek/reek_sim_zone.roff", grid=grid
+    )
+    gridzones.name = "Zone"
+
+    well = loadwell1
+    well.get_gridproperties(gridzones, grid)
+    well.zonelogname = "Zone_model"
+
+    assert well.get_logrecord(well.zonelogname) == {
+        1: "Below_Top_reek",
+        2: "Below_Mid_reek",
+        3: "Below_Low_reek",
+    }
