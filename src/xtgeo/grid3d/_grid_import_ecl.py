4bebec88b1df742e4890b3befff386e06fc97036
mferrera <matt.ferrera@gmail.com>
2024-01-05 13:50:02 +0100

    CLN: add typing to `_grid_import_ecl`

--- src/xtgeo/grid3d/_grid_import_ecl.py
+++ src/xtgeo/grid3d/_grid_import_ecl.py
@@ -1,21 +1,27 @@
 """Grid import functions for Eclipse, new approach (i.e. version 2)."""
+from __future__ import annotations
 
+from typing import TYPE_CHECKING, Any, Literal
 
-import xtgeo
 from xtgeo.common import null_logger
+from xtgeo.common.sys import _XTGeoFile
 from xtgeo.grid3d._egrid import EGrid, RockModel
 from xtgeo.grid3d._grdecl_grid import GrdeclGrid, GridRelative
+from xtgeo.grid3d.grid_properties import GridProperties, gridproperties_from_file
 
 logger = null_logger(__name__)
 
 
-def import_ecl_egrid(
-    gfile,
-    relative_to=GridRelative.MAP,
-    fileformat="egrid",
-):
-    egrid = EGrid.from_file(gfile._file, fileformat=fileformat)
+if TYPE_CHECKING:
+    from xtgeo.grid3d.grid import Grid
 
+
+def import_ecl_egrid(
+    gfile: _XTGeoFile,
+    relative_to: GridRelative = GridRelative.MAP,
+    fileformat: Literal["egrid", "fegrid"] = "egrid",
+) -> dict[str, Any]:
+    egrid = EGrid.from_file(gfile.file, fileformat=fileformat)
     result = grid_from_ecl_grid(egrid, relative_to=relative_to)
 
     if egrid.egrid_head.file_head.rock_model == RockModel.DUAL_POROSITY:
@@ -28,58 +34,60 @@ def import_ecl_egrid(
     return result
 
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Import eclipse run suite: EGRID + properties from INIT and UNRST
-# For the INIT and UNRST, props dates shall be selected
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def import_ecl_run(
-    groot, ecl_grid, initprops=None, restartprops=None, restartdates=None
-):
-    """Import combo ECL runs."""
-    ecl_init = groot + ".INIT"
-    ecl_rsta = groot + ".UNRST"
-
-    ecl_init = xtgeo._XTGeoFile(ecl_init)
-    ecl_rsta = xtgeo._XTGeoFile(ecl_rsta)
-
-    grdprops = xtgeo.grid3d.GridProperties()
+    groot: str,
+    ecl_grid: Grid,
+    initprops: list[str] | None = None,
+    restartprops: list[str] | None = None,
+    restartdates: list[str] | None = None,
+) -> None:
+    """Import Eclipse run suite: EGrid and properties from INIT and UNRST.
+    For the INIT and UNRST files, property dates shall be selected."""
+    ecl_init = _XTGeoFile(f"{groot}.INIT")
+    ecl_rsta = _XTGeoFile(f"{groot}.UNRST")
+    grdprops = GridProperties()
 
     # import the init properties unless list is empty
     if initprops:
-        initprops = xtgeo.gridproperties_from_file(
+        initprops_ = gridproperties_from_file(
             ecl_init.name, names=initprops, fformat="init", dates=None, grid=ecl_grid
         )
-        grdprops.append_props(initprops.props)
+        if initprops_.props:
+            grdprops.append_props(initprops_.props)
 
     # import the restart properties for dates unless lists are empty
     if restartprops and restartdates:
-        restartprops = xtgeo.gridproperties_from_file(
+        restartprops_ = gridproperties_from_file(
             ecl_rsta.name,
             names=restartprops,
             fformat="unrst",
             dates=restartdates,
             grid=ecl_grid,
         )
-        grdprops.append_props(restartprops.props)
-
+        if restartprops_.props:
+            grdprops.append_props(restartprops_.props)
     ecl_grid.gridprops = grdprops
 
 
-def import_ecl_grdecl(gfile, relative_to=GridRelative.MAP):
+def import_ecl_grdecl(
+    gfile: _XTGeoFile, relative_to: GridRelative = GridRelative.MAP
+) -> dict[str, Any]:
     """Import grdecl format."""
-
-    grdecl_grid = GrdeclGrid.from_file(gfile._file, fileformat="grdecl")
+    grdecl_grid = GrdeclGrid.from_file(gfile.file, fileformat="grdecl")
     return grid_from_ecl_grid(grdecl_grid, relative_to=relative_to)
 
 
-def import_ecl_bgrdecl(gfile, relative_to=GridRelative.MAP):
+def import_ecl_bgrdecl(
+    gfile: _XTGeoFile, relative_to: GridRelative = GridRelative.MAP
+) -> dict[str, Any]:
     """Import binary files with GRDECL layout."""
-
-    grdecl_grid = GrdeclGrid.from_file(gfile._file, fileformat="bgrdecl")
+    grdecl_grid = GrdeclGrid.from_file(gfile.file, fileformat="bgrdecl")
     return grid_from_ecl_grid(grdecl_grid, relative_to=relative_to)
 
 
-def grid_from_ecl_grid(ecl_grid, relative_to=GridRelative.MAP):
+def grid_from_ecl_grid(
+    ecl_grid: EGrid, relative_to: GridRelative = GridRelative.MAP
+) -> dict[str, Any]:
     result = {}
     result["coordsv"] = ecl_grid.xtgeo_coord(relative_to=relative_to)
     result["zcornsv"] = ecl_grid.xtgeo_zcorn(relative_to=relative_to)
