9e73cde24ccfa3868bc45d281ca2d71f7a1108a9
Sindre Langeveld <slang@equinor.com>
2025-01-24 12:55:14 +0100

    CI: Use xtgeo-testdata from cache

--- ci/testrmsenv.sh
+++ ci/testrmsenv.sh
@@ -17,14 +17,6 @@ set_test_variables() {
 }
 
 copy_test_files () {
-    echo "Copy xtgeo testdata..."
-    if ! [ -d $XTGEO_TESTDATA_PATH ]; then
-        echo "Cloning xtgeo-testdata to $XTGEO_TESTDATA_PATH..."
-        git clone --depth=1 https://github.com/equinor/xtgeo-testdata $XTGEO_TESTDATA_PATH
-    else
-        echo "xtgeo testdata already exists at path $XTGEO_TESTDATA_PATH. Skipping copy."
-    fi
-    
     echo "Copy xtgeo test files to $CI_TEST_ROOT..."
     mkdir -p $CI_TEST_ROOT
     cp -r $PROJECT_ROOT/tests $CI_TEST_ROOT
@@ -46,6 +38,6 @@ install_test_dependencies () {
 run_pytest () {
     echo "Running xtgeo tests with pytest..."
     pushd $CI_TEST_ROOT
-    pytest -n 4 -vv
+    pytest ./tests -n 4 -vv --testdatapath $XTGEO_TESTDATA_PATH
     popd
 }
