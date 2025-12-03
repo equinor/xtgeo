f9e333760cb68eac4139a9a55b1d800de94d13d1
mferrera <13275515+mferrera@users.noreply.github.com>
2025-12-03 09:07:51 +0100

    TST: Refactor fork test

--- tests/test_surface/test_forks.py
+++ tests/test_surface/test_forks.py
@@ -1,31 +1,33 @@
 import logging
 import subprocess
+import sys
+
+import pytest
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def test_surface_forks():
+@pytest.mark.skipif(sys.platform == "darwin", reason="Multiprocessing issues on macOS")
+def test_surface_forks() -> None:
     """Testing when surfaces are read by multiple forks"""
 
     process = subprocess.Popen(
-        ["python", "multiprocess_surfs.py"],
+        [sys.executable, "multiprocess_surfs.py"],
         cwd="examples",
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
     )
-    try:
-        stdout, stderr = process.communicate(timeout=60)  # timeout to prevent hanging
-        ret_code = process.returncode
-    except subprocess.TimeoutExpired:
-        process.kill()
-        stdout, stderr = process.communicate()
-        ret_code = -1  # Use a custom return code for timeouts
+    stdout, stderr = process.communicate(timeout=60)
+    ret_code = process.returncode
 
-        # Log more information for debugging
+    if ret_code != 0:
         logger.info("Return code: %d", ret_code)
         logger.info("STDOUT: %s", stdout.decode())
         logger.info("STDERR: %s", stderr.decode())
 
-    assert ret_code == 0, f"Subprocess failed with exit code {ret_code}\n"
-    f"STDOUT: {stdout.decode()}\nSTDERR: {stderr.decode()}"
+    assert ret_code == 0, (
+        f"Subprocess failed with exit code {ret_code}\n"
+        f"STDOUT: {stdout.decode()}\n"
+        f"STDERR: {stderr.decode()}"
+    )
