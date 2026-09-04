8558426230c074f98543832a364cb986b47d046d
Adil Khurshid <AKHUR@equinor.com>
2026-09-04 12:47:18 +0200

    MAINT: Reformat C/C++ for clang-format 23.1.0
    Clang-format 22.1.8 -> 23.1.0, whose updated rules flag the
    previously-compliant formatting as violations. Reformat the affected
    sources so the linting workflow passes with the new version.

--- src/lib/src/x_3d_polyhedron.c
+++ src/lib/src/x_3d_polyhedron.c
@@ -4,53 +4,52 @@
 #include <xtgeo/xtgeo.h>
 #include "common.h"
 
-const int TETRACOMBS[4][6][4] = {
-    // cell top/base hinge is splittet 0 - 3 / 4 - 7
-    {
-      // lower right common vertex 5
-      { 3, 7, 4, 5 },
-      { 0, 4, 7, 5 },
-      { 0, 3, 1, 5 },
-      // upper left common vertex 6
-      { 0, 4, 7, 6 },
-      { 3, 7, 4, 6 },
-      { 0, 3, 2, 6 },
-    },
+const int TETRACOMBS[4][6][4] = { // cell top/base hinge is splittet 0 - 3 / 4 - 7
+                                  {
+                                    // lower right common vertex 5
+                                    { 3, 7, 4, 5 },
+                                    { 0, 4, 7, 5 },
+                                    { 0, 3, 1, 5 },
+                                    // upper left common vertex 6
+                                    { 0, 4, 7, 6 },
+                                    { 3, 7, 4, 6 },
+                                    { 0, 3, 2, 6 },
+                                  },
 
-    // cell top/base hinge is splittet 1 -2 / 5- 6
-    {
-      // upper right common vertex 7
-      { 1, 5, 6, 7 },
-      { 2, 6, 5, 7 },
-      { 1, 2, 3, 7 },
-      // lower left common vertex 4
-      { 1, 5, 6, 4 },
-      { 2, 6, 5, 4 },
-      { 1, 2, 0, 4 },
-    },
+                                  // cell top/base hinge is splittet 1 -2 / 5- 6
+                                  {
+                                    // upper right common vertex 7
+                                    { 1, 5, 6, 7 },
+                                    { 2, 6, 5, 7 },
+                                    { 1, 2, 3, 7 },
+                                    // lower left common vertex 4
+                                    { 1, 5, 6, 4 },
+                                    { 2, 6, 5, 4 },
+                                    { 1, 2, 0, 4 },
+                                  },
 
-    // Another combination...
-    // cell top/base hinge is splittet 0 - 3 / 4 - 7
-    {
-      // lower right common vertex 1
-      { 3, 7, 0, 1 },
-      { 0, 4, 3, 1 },
-      { 4, 7, 5, 1 },
-      // upper left common vertex 2
-      { 0, 4, 3, 2 },
-      { 3, 7, 0, 2 },
-      { 4, 7, 6, 2 },
-    },
+                                  // Another combination...
+                                  // cell top/base hinge is splittet 0 - 3 / 4 - 7
+                                  {
+                                    // lower right common vertex 1
+                                    { 3, 7, 0, 1 },
+                                    { 0, 4, 3, 1 },
+                                    { 4, 7, 5, 1 },
+                                    // upper left common vertex 2
+                                    { 0, 4, 3, 2 },
+                                    { 3, 7, 0, 2 },
+                                    { 4, 7, 6, 2 },
+                                  },
 
-    // cell top/base hinge is splittet 1 -2 / 5- 6
-    { // upper right common vertex 3
-      { 1, 5, 2, 3 },
-      { 2, 6, 1, 3 },
-      { 5, 6, 7, 3 },
-      // lower left common vertex 0
-      { 1, 5, 2, 0 },
-      { 2, 6, 1, 0 },
-      { 5, 6, 4, 0 } }
+                                  // cell top/base hinge is splittet 1 -2 / 5- 6
+                                  { // upper right common vertex 3
+                                    { 1, 5, 2, 3 },
+                                    { 2, 6, 1, 3 },
+                                    { 5, 6, 7, 3 },
+                                    // lower left common vertex 0
+                                    { 1, 5, 2, 0 },
+                                    { 2, 6, 1, 0 },
+                                    { 5, 6, 4, 0 } }
 
 };
 
